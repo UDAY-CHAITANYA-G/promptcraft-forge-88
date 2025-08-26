fc789c50eafe0220adfc24366be5e6fa0840e0f1
UDAY-CHAITANYA-G <udaychaitanyagurvindapalli@gmail.com>
2025-08-26 22:23:54 +0530

    attached emailjs config

---
+++ src/lib/emailConfig.private.ts
@@ -0,0 +1,19 @@
+// PRIVATE EmailJS Configuration - DO NOT COMMIT TO VERSION CONTROL
+// This file contains sensitive credentials and should be kept private
+
+export const emailConfigPrivate = {
+  // Your EmailJS service ID (found in EmailJS dashboard under Email Services)
+  serviceId: 'service_38iu102',
+  
+  // Your EmailJS template ID (found in EmailJS dashboard under Email Templates)
+  templateId: 'template_jo4pt1s',
+  
+  // Your EmailJS public key (found in EmailJS dashboard under Account > API Keys)
+  publicKey: 'OruE_DJ9e1VntDm74',
+  
+  // The email address where feedback will be sent
+  recipientEmail: 'zeroxchaitanya@gmail.com',
+
+};
+
+// Note: This file should be added to .gitignore to prevent committing sensitive data
